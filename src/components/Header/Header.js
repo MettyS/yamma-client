09aae2d005995dbc1a6450229816ccc179853f28
Nate Voisan <rufio96@gmail.com>
2021-01-04 15:26:37 -0800

    mobile css done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import YammaLogo from '../../images/YammaLogo.png';
+import yammalogored from '../../images/yammalogored.PNG';
 import TokenService from '../../services/token-service';
 
 class Header extends Component {
@@ -22,38 +22,58 @@ class Header extends Component {
     );
   }
 
-    renderHomeLink() {
-        return (
-            <nav>
-                <ul className="home-links">
-                    <li><Link to='/aboutus'>About</Link></li>
-                    <li className='bars'>|</li>
-                    <li><Link to='/important'>Important Notice</Link></li>
-                    <li className='bars'>|</li>
-                    <li><Link to='/contactus'>Contact</Link></li>
-                </ul>
-            </nav>
-        )
-    }
+  renderLoginLink() {
+    return (
+      <nav className='login-links'>
+        <Link to='/login'>
+          <button className='user-button'>Login</button>
+        </Link>{' '}
+        <br></br>
+        <Link to='/register'>
+          <button className='user-button'>Sign up</button>
+        </Link>
+      </nav>
+    );
+  }
 
-    render() {
-        return (
-            <header className="header">
-                <div>
-                    <Link to='/' className="title">
-                        <img className="logo" src={YammaLogo} alt="Yamma-Logo" />
-                        <h1>Yamma</h1>
-                    </Link>
-                </div>
+  renderHomeLink() {
+    return (
+      <nav>
+        <ul className='home-links'>
+          <li>
+            <Link to='/aboutus'>About</Link>
+          </li>
+          <li className='bars'>|</li>
+          <li>
+            <Link to='/important'>Important Notice</Link>
+          </li>
+          <li className='bars'>|</li>
+          <li>
+            <Link to='/contactus'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 
-              <div>{this.renderHomeLink()}</div>
+  render() {
+    return (
+      <header className='header'>
+        <div>
+          <Link to='/' className='title'>
+            <img className='logo' src={yammalogored} alt='Yamma-Logo' />
+            <h1>Yamma</h1>
+          </Link>
+        </div>
 
-              <div>
-                {TokenService.hasAuthToken()
-                  ? this.renderLogoutLink()
-                  : this.renderLoginLink()}
-              </div>
-            </header>
+        <div>{this.renderHomeLink()}</div>
+
+        <div>
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </div>
+      </header>
     );
   }
 }
