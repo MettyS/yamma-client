9c84eb91eb54c0971e0f161afc274b22a5a833a4
Nate Voisan <rufio96@gmail.com>
2021-01-08 13:48:56 -0800

    article/chat page styling and js editing

--- src/components/EventCard.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class EventCard extends Component {
-  render() {
-    return <div className='eventcard-container'>Implement me!</div>;
-  }
-}
-
-export default EventCard;
