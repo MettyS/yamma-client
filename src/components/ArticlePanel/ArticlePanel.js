7b62ef58a7687d8ff96ed11aca12de89016857a6
Nate Voisan <rufio96@gmail.com>
2021-01-29 15:49:11 -0800

    fixed all the warnings in the console

--- src/components/ArticlePanel/ArticlePanel.js
+++ src/components/ArticlePanel/ArticlePanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import ArticleCard from '../ArticleCard/ArticleCard'
 import './ArticlePanel.css';
 //import EventContext from '../../context/EventContext';
--- src/routes/DashboardRoute.js
+++ src/routes/DashboardRoute.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import ArticlePanel from '../components/ArticlePanel/ArticlePanel';
 //import { UserProvider } from '../context/UserContext'
 //import EventContext from '../context/EventContext'
 import CategoryList from '../components/CategoryList/CategoryList';
-import CategoryPage from '../components/CategoryPage/CategoryPage';
+// import CategoryPage from '../components/CategoryPage/CategoryPage';
 
 class DashBoard extends Component {
+  
   renderArticlePanel() {
-    console.log('dashboard re-render!');
     return (
-      
           <>
           {['/', '/event/article/:eventId/:title'].map(path => (
 
@@ -22,31 +21,8 @@ class DashBoard extends Component {
               )}/> 
             ) )
           }
-                  
-            
-          
-
         </>
     )
-  }
-
-  renderCategoryList() {
-    return(
-      <>
-        {['/', '/event/article/:title'].map((path) => (
-          <Route
-            key={path}
-            path={path}
-            render={() => (
-              <ArticlePanel />
-              // <EventContext.Consumer>
-              //   { val => <ArticlePanel /> }
-              // </EventContext.Consumer>
-            )}
-          />
-        ))}
-      </>
-    );
   }
 
   renderCategoryList() {
