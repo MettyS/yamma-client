0308cbd1b212d31de688841b0a0d321136965382
Metty Schroeder <mettyj.schroeder@gmail.com>
2021-01-29 21:03:32 -0500

    got rid of warnings

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Footer.css';
-import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   mybutton = document.getElementById('myBtn');
